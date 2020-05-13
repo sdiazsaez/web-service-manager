aa95e405d3162b4dc99c7c3720f01ea40f7dfbe9
simon <sdiaz.sz@gmail.com>
2020-05-13 06:05:53 -0400

    move transformableRequest psr4fix

--- src/Request/TransformableRequest.php
+++ src/Request/TransformableRequest.php
@@ -4,6 +4,8 @@
  * Point where the service is able to format the provided data
  * to comply with service request structure
  */
+
+//move to Adapters and update providers
 namespace Larangular\WebServiceManager\Request;
 
 interface TransformableRequest {
